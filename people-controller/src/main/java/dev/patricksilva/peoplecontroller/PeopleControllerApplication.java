eb60932c54e88347af393a630920d315787418d1
patricksilva1 <patricksilva5160@gmail.com>
2022-11-06 21:00:14 -0300

    pom and MS1 fix properties

--- people-controller/src/main/java/dev/patricksilva/peoplecontroller/PeopleControllerApplication.java
+++ people-controller/src/main/java/dev/patricksilva/peoplecontroller/PeopleControllerApplication.java
@@ -3,10 +3,13 @@ package dev.patricksilva.peoplecontroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableFeignClients
+@EnableFeignClients
+@EnableJpaRepositories
 public class PeopleControllerApplication {
 
 	public static void main(String[] args) {
--- people-controller/src/main/java/dev/patricksilva/peoplecontroller/repository/PeopleRepository.java
+++ people-controller/src/main/java/dev/patricksilva/peoplecontroller/repository/PeopleRepository.java
@@ -1,11 +1,13 @@
 package dev.patricksilva.peoplecontroller.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import dev.patricksilva.peoplecontroller.model.People;
 
 @Repository
+@EnableJpaRepositories
 public interface PeopleRepository extends JpaRepository<People, String>{
 
 }
