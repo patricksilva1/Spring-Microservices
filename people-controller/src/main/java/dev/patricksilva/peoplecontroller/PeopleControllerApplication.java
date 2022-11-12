d0528435696809b41a670fc9075ebf21f7e6ef93
patricksilva1 <patricksilva5160@gmail.com>
2022-11-11 22:08:23 -0300

    fix controller

--- people-controller/src/main/java/dev/patricksilva/peoplecontroller/PeopleControllerApplication.java
+++ people-controller/src/main/java/dev/patricksilva/peoplecontroller/PeopleControllerApplication.java
@@ -3,13 +3,11 @@ package dev.patricksilva.peoplecontroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@EnableJpaRepositories
 public class PeopleControllerApplication {
 
 	public static void main(String[] args) {
