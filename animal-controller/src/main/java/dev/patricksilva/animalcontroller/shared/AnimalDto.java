e5b73cc12e1d2c25fb0e9279cfe76f64894019eb
patricksilva1 <patricksilva5160@gmail.com>
2022-11-14 21:43:20 -0300

    animal dto

---
+++ animal-controller/src/main/java/dev/patricksilva/animalcontroller/shared/AnimalDto.java
@@ -0,0 +1,62 @@
+package dev.patricksilva.animalcontroller.shared;
+
+public class AnimalDto {
+    private Integer id;
+    private Integer own;
+    private String name;
+    private Integer age;
+    private String breed;
+    private Boolean alive;
+
+    // #region Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOwn() {
+        return own;
+    }
+
+    public void setOwn(Integer own) {
+        this.own = own;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Boolean getAlive() {
+        return alive;
+    }
+
+    public void setAlive(Boolean alive) {
+        this.alive = alive;
+    }
+
+    // #endregion
+}
