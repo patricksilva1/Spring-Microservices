99c51e58f3e2bc1168ee4343a2ac932b831069f0
patricksilva1 <patricksilva5160@gmail.com>
2022-11-14 21:22:59 -0300

    fix validHandler

--- people-controller/src/main/java/dev/patricksilva/peoplecontroller/view/handlers/ValidationHandler.java
+++ people-controller/src/main/java/dev/patricksilva/peoplecontroller/view/handlers/ValidationHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class ValidationHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
